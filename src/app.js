eb83d5a8ed847f05321ec3d55f41d9bc1f98a851
侯本涛 <houbentao@MacBook-Pro.local>
2020-02-22 10:08:37 +0800

    addpost is done ,ready for update

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const bodyInput = document.querySelector('#body').value;
 const idInput = document.querySelector('#id').value;
 //event listener
 document.addEventListener('DOMContentLoaded',getPosts)
+document.querySelector('.post-submit').addEventListener('click',submitPost)
 
 //get posts
 
@@ -14,4 +15,23 @@ function getPosts(){
     ui.showPosts(data)
     ui.showAlert('post is getted','alert alert-success')
   }).catch(err =>console.log(err))
+}
+
+function submitPost(){
+  const title = document.querySelector('#title').value
+  const body = document.querySelector('#body').value
+  const id = document.querySelector('#id').value
+  if( title === '' || body === ''){
+    ui.showAlert('check input','alert alert-danger')
+  }else{
+    const data={
+      title,
+      body
+    }
+    http.post(`http://localhost:3000/posts/${id}`,data)
+    getPosts()
+    ui.showAlert('a  new post is added ','alert alert-success')
+    ui.clearFields();
+  }
+
 }
--- src/ui.js
+++ src/ui.js
@@ -1,26 +1,12 @@
 class UI {
   constructor() {
-
+    this.titleInput=document.querySelector('#title')
+    this.bodyInput=document.querySelector('#body')
+    this.idInput=document.querySelector('#id')
   }
   showPosts(posts) {
     let output = ''
     posts.forEach(post => {
-      //   output=`
-      //   <div class="card mb-3">
-      //   <div class="card-body">
-      //     <h4 class="card-title">${post.title}</h4>
-      //     <p class="card-text">${post.body}</p>
-      //     <a href="#" class="edit card-link" data-id="${post.id}">
-      //       <i class="fa fa-pencil"></i>
-      //     </a>
-
-      //     <a href="#" class="delete card-link" data-id="${post.id}">
-      //     <i class="fa fa-remove"></i>
-      //   </a>
-      //   </div>
-      // </div>
-
-      //   `
       output += `
       <div class="card">
         <div class="card-body">
@@ -59,6 +45,11 @@ class UI {
       currentAlert.remove();
     }
   }
+  clearFields(){
+    this.titleInput.value=''
+    this.bodyInput.value=''
+  }
+  
 }
 
 export const ui = new UI()
